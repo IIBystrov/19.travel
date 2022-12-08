4c939f4f84e651fabf5860be10842e537c539b42
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-08 13:22:08 +0300

    Add showModal (script.js)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 let introBtn = document.querySelector(".intro__btn");
 let modalWindow = document.querySelector(".modal");
+let modalWindowActive = document.querySelector(".modal--active");
+
+function showModal() {
+    modalWindow.classList.add("modal--active");
+}
