93bacc1fe11963e3f608b97397fef84a17e0d9dd
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-08 13:39:18 +0300

    Add ev.list introBtn (script.js)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let introBtn = document.querySelector(".intro__btn");
 let modalWindow = document.querySelector(".modal");
 let modalWindowActive = document.querySelector(".modal--active");
 
+introBtn.addEventListener("click", showModal);
+
 function showModal() {
     modalWindow.classList.add("modal--active");
 }
