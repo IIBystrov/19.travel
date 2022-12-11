5b2b3b3d0f9c7be7a748600ed7c9c9fe4fa6cf5a
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-11 12:23:35 +0300

    Add btnLineMiddle (script.js)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ let modalWindow = document.querySelector(".modal");
 let modalWindowActive = document.querySelector(".modal--active");
 let navBtn = document.querySelector(".nav__btn");
 let btnLine = document.querySelectorAll(".btn__line");
+let btnLineTop = btnLine[1];
+let btnLineMiddle = btnLine[2];
 
 navBtn.addEventListener("click", showNav);
 
