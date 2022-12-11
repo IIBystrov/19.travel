79289a949428d2acab5dfb335de898769725100e
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-11 12:19:26 +0300

    animNavBtn (script.js)

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ let modalWindowActive = document.querySelector(".modal--active");
 
 let navBtn = document.querySelector(".nav__btn");
 let btnLine = document.querySelectorAll(".btn__line");
-let btnLineTop = btnLine[1];
-let btnLineMiddle = btnLine[2];
-let btnLineBottom = btnLine[3];
+let btnLineTop = btnLine[0];
+let btnLineMiddle = btnLine[1];
+let btnLineBottom = btnLine[2];
 
 navBtn.addEventListener("click", animNavBtn);
 
 function animNavBtn() {
     btnLineTop.classList.toggle("btn__line--top");
+    btnLineMiddle.classList.toggle("btn__line--middle");
+    btnLineBottom.classList.toggle("btn__line--bottom");
 }
 
 introBtn.addEventListener("click", showModal);
